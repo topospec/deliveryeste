dca4a30ff21bce7a5395cf745ff7c66e26cb79da
caparrosmatias <caparrosmatias@users.noreply.github.com>
2020-04-05 16:32:15 -0300

    3 boton

--- facebookSDK.js
+++ facebookSDK.js
@@ -42,7 +42,7 @@ var Facebook_correctLogin	= function () {
 		// Saludar al usuario
 		if (document.getElementById('fbStatus')){
 			document.getElementById('fbStatus').innerHTML = '¡Bienvenido ' + facebookUser.name + '!'
-			document.getElementById('next').style.display = "block";
+			document.getElementById("next").disabled = false;
 		}
 		// Mostrar los datos en la consola
 		console.log('____________________');
