b1db1ccf7f360e6e52988eeaa3e572e7be57b6b1
caparrosmatias <caparrosmatias@users.noreply.github.com>
2020-04-05 15:45:08 -0300

    domingo

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from flask import Flask, render_template, request
-from twilio.rest import Client
-
-#Credenciales Twilio
-account_sid = 'AC62820f71bb34cfc5cfd97bd977c7ddc8'
-auth_token = '40b45351cfe6ca60a3a246909a019b13'
-client = Client(account_sid, auth_token)
 
 #Inicio Flask
 app = Flask(__name__)
@@ -13,27 +7,8 @@ DEBUG = False
 
 
 @app.route("/", methods=['GET', 'POST'])
-def pedir():
-    print(request.method)
-    if request.method == 'POST':
-        if request.form.get('enviar') == 'enviar':
-            print("Pedido enviado!")
-            if request.form.get('negocio') == 'Topo':
-                numero = "5492634665255"
-            elif request.form.get('negocio') == 'Perro':
-                numero = "5492634795709"
-            solicitud = request.form.get('solicitud')
-            enviar(solicitud,numero)
-    return render_template("pedir.html")
-
-def enviar(mensaje,numero):
-    message = client.messages.create(
-                              body=str(mensaje), #Variablejjjkkk
-                              from_='whatsapp:+14155238886', 
-                              to='whatsapp:+'+numero #numero a donde estoy enviando el mensaje
-                          )
-    print(message.sid)
-
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(port = PORT, debug = DEBUG)
